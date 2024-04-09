6d72b121570758331e315cf9e2815735865593f3
n.perovic <n.perovic@sportradar.com>
2024-04-09 22:02:06 +0200

    Reorganization

--- src/main/java/app/HandelbankenCodingChalenge.java
+++ src/main/java/app/HandelbankenCodingChalenge.java
@@ -2,7 +2,11 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "ctrlEntityManagerFactory",
+        transactionManagerRef = "ctrlTransactionManager")
 @SpringBootApplication
 public class HandelbankenCodingChalenge {
     public static void main(String[] args) {
